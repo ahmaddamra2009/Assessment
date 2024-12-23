83b21f629cc13c4125c20f77c8fbda7925e8904c
Ahmad Damra <ahmeddamra_cs@hotmail.com>
2024-12-23 20:30:41 +0300

    Add project files.

---
+++ ConsoleApp3/DemoClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class DemoClass
+    {
+        //Vowel Count
+        public int CountVowels(string word)
+        {
+            int count = 0;
+            string vowels = "aeiouAEIOU";
+
+            foreach (char c in word)
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        //Simple adding
+        public int SimpleAdding(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        // factorial
+        public long FirstFactorial(int num)
+        {
+            long factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        // Simple Evens
+        public string SimpleEvens(string num)
+        {
+            foreach (char digit in num)
+            {
+                // Check if the character is not even
+                if ((digit - '0') % 2 != 0)
+                {
+                    return "false";
+                }
+            }
+            return "true";
+        }
+
+        // OneDecremented
+        public int OneDecremented(string str)
+        {
+            int count = 0;
+            // 987776
+            // Loop through the string from the second character
+            for (int i = 1; i < str.Length; i++)
+            {
+                // Check if the current digit is one less than the previous digit
+                if ((str[i - 1]) - 1 == (str[i]))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleApp3;
+using System;
+#region VowelCount
+
+class Program
+{
+    static void Main()
+    {
+
+        DemoClass demo = new DemoClass();
+        // Vowel
+        Console.WriteLine("-------------Vowel ");
+        Console.WriteLine("Enter a word:");
+        string input = Console.ReadLine();
+        
+        int vowelCount = demo.CountVowels(input);
+        Console.WriteLine($"The number of vowels in the word is: {vowelCount}");
+        // End Vower
+
+        // Simple Adding
+        Console.WriteLine("---------Simple Adding");
+        Console.WriteLine("Enter a number:");
+        int num = int.Parse(Console.ReadLine());
+
+        int result = demo.SimpleAdding(num);
+        Console.WriteLine($"The sum of numbers from 1 to {num} is: {result}");
+
+        // Factorial
+
+        Console.WriteLine("------------- Factorial:");
+        Console.WriteLine("Enter a number:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        long result1 = demo.FirstFactorial(num1);
+        Console.WriteLine($"The factorial of {num1} is: {result1}");
+
+        // SimpleEvens
+        Console.WriteLine("------------ SimpleEvens");
+        Console.WriteLine("Enter a number:");
+        string input1 = Console.ReadLine();
+
+        string result2 = demo.SimpleEvens(input);
+        Console.WriteLine($"Are all digits even? {result2}");
+
+        // OneDecremented
+        Console.WriteLine("--------------OneDecremented");
+        Console.WriteLine("Enter a string of digits:");
+        string OneDecrementedinput = Console.ReadLine();
+
+        int OneDecrementedresult = demo.OneDecremented(OneDecrementedinput);
+        Console.WriteLine($"Count of digits one less than the previous: {OneDecrementedresult}");
+    }
+
+
+}
+#endregion
+
+
+
+
+    
